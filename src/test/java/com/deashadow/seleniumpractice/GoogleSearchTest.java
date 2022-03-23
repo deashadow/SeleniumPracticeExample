aaed56360ea20a8ad9e4e44cbac158c2e0333e16
deashadow <67285924+deashadow@users.noreply.github.com>
2022-03-23 17:24:53 -0400

    changed chromedriver set-up

--- src/test/java/com/deashadow/seleniumpractice/GoogleSearchTest.java
+++ src/test/java/com/deashadow/seleniumpractice/GoogleSearchTest.java
@@ -2,24 +2,34 @@ package com.deashadow.seleniumpractice;
 
 import static org.junit.Assert.assertEquals;
 import java.util.concurrent.TimeUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.logging.Logger;
 
 public class GoogleSearchTest {
-    private WebDriver driver;
+    private static WebDriver driver;
     private static final Logger LOGGER = Logger.getLogger(GoogleSearchTest.class.getName());
 
-    @Before
-    public void setUp() {
-        System.setProperty("webdriver.gecko.driver", "./src/test/resources/firefoxdriver/geckodriver.exe");
-        driver = new FirefoxDriver();
+    @BeforeClass
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.google.com");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.close();
     }
 
     @Test
